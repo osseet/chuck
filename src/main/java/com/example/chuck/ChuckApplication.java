77004eef3103f8d504092244d84e5014ae2a06d0
uko <osseet@hotmail.com>
2018-11-17 19:33:02 +0100

    first commit

---
+++ src/main/java/com/example/chuck/ChuckApplication.java
@@ -0,0 +1,12 @@
+package com.example.chuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckApplication.class, args);
+	}
+}
